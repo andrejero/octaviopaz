433bb820441aa052733ea85f752ddcea16431a4e
andrejero <143059918+andrejero@users.noreply.github.com>
2024-02-15 13:36:33 -0600

    Add files via upload

---
+++ Arreglos/src/main/java/com/mycompany/arreglos/Arreglos.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.arreglos;
+
+
+public class Arreglos {
+
+    public static void main(String[] args) {
+        int [] numeros = {12,10,4,24,5,9};
+        
+        System.out.println("numeros[0]");
+        
+       for(int i=0; i<numeros.length;i++){
+           System.out.println("el arreglo en la posicion:" + i + "tiene el numero:" + numeros);
+       }
+        
+    }
+}
