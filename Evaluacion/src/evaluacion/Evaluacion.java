800707de43ca0b55849345c28774f58f3d708445
andrejero <143059918+andrejero@users.noreply.github.com>
2024-02-15 13:38:31 -0600

    Add files via upload

---
+++ Evaluacion/src/evaluacion/Evaluacion.java
@@ -0,0 +1,14 @@
+
+package evaluacion;
+
+
+public class Evaluacion {
+
+ 
+    public static void main(String[] args) {
+       //el método main ejecuta las tareas de la clase
+       
+        System.out.println("¡mi primer programa java desce cero!");
+    }
+    
+}
