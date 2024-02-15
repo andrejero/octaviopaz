4c452dc3215cbbf5ba8e776baad9cda6026a7dde
andrejero <143059918+andrejero@users.noreply.github.com>
2024-02-15 13:37:47 -0600

    Add files via upload

---
+++ Char/src/pkgchar/Char.java
@@ -0,0 +1,14 @@
+
+package pkgchar;
+
+
+public class Char {
+
+    
+    public static void main(String[] args) {
+        char primerLetra='A';
+        
+            System.out.println(primerLetra);
+    }
+    
+}
