7978d83075801481b83cd8eb073583f1b8b31439
andrejero <143059918+andrejero@users.noreply.github.com>
2024-08-24 18:15:24 -0600

    Add files via upload

---
+++ holaMundo/js/main.js
@@ -0,0 +1,50 @@
+//alert("Hola mundo");
+var nombre = "Andrea";
+var apellido = "Jeronimo";
+var edad = 21;
+var inscritoEnTaller = true;
+var calificacionPrevia = "7.8";
+ console.log("Nombre: " + nombre + " Es de tipo " + typeof nombre);
+ console.log("Apellido: " + apellido + " Es de tipo " + typeof apellido);
+ console.log("Edad: " + edad + " Es de tipo " + typeof edad);
+ console.log("Inscrito en taller: " + inscritoEnTaller + " Es de tipo " + typeof inscritoEnTaller );
+ console.log("Calificacion: " + calificacionPrevia  + " Es de tipo " + typeof calificacionPrevia);
+
+ if(true){
+let peso = 43.5;
+console.log("peso:" + peso);
+ }
+ 
+ var suma;
+ var resta;
+ var multiplicacion;
+ var division;
+ var residuo;
+
+ suma = 3 + 45;
+ console.log(suma);
+ resta = 87 - 12;
+ console.log(resta);
+ multiplicacion = 8 * 2;
+ console.log(multiplicacion);
+ division = 56 / 0;
+ console.log(division);
+ residuo = 286 % 7;
+ console.log(residuo);
+
+ var edad1 =15;
+ edad1++
+ edad1--
+ console.log(edad1);
+
+ //Operadores relacionales o de comparacion
+ console.log(5!=4);
+
+ //operadores logicos
+ var ine = true;
+ var mayor=17;
+ console.log(ine==true || mayor > 18);
+
+
+ 
+ 
