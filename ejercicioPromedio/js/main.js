e3d298e74f5880bce9b5ce10c6b572bcd6e16167
andrejero <143059918+andrejero@users.noreply.github.com>
2024-08-24 18:35:51 -0600

    ejerciciopromediojs

--- ejercicioPromedio/js/main.js
+++ ejercicioPromedio/js/main.js
@@ -1,4 +1,3 @@
-//variables
 var periodo1;
 var periodo2;
 var periodo3;
@@ -6,19 +5,29 @@ var taller;
 var computo;
 var promedioGeneral;
 
-
-taller=6.7;
-computo=8.0;
+taller= parseInt(prompt("Ingresa la calificacion de taller"));
+computo= parseInt(prompt("Ingresa la calificacion de computo"));
 periodo1=(taller + computo )/2;
 console.log("El promedio del periodo 1 es:" + periodo1);
-taller=5.7;
-computo=9.0;
+
+taller= parseInt(prompt("Ingresa la calificacion de taller"));
+computo= parseInt(prompt("Ingresa la calificacion de computo"));
 periodo2=(taller + computo )/2;
-console.log("El promedio del periodo 2 es:" + periodo2);
-taller=9.92;
-computo=6.8;
+console.log("El promedio del periodo 1 es:" + periodo2);
+
+taller= parseInt(prompt("Ingresa la calificacion de taller"));
+computo= parseInt(prompt("Ingresa la calificacion de computo"));
 periodo3=(taller + computo )/2;
-console.log("El promedio del periodo 3 es:" + periodo3);
+console.log("El promedio del periodo 1 es:" + periodo3);
+
 promedioGeneral=(periodo1 + periodo2 + periodo3) /3;
 console.log("El promedio general es:" + promedioGeneral);
-console.log("APROBADO" + promedioGeneral > 6);
+//console.log("APROBADO" + promedioGeneral > 6);
+
+
+
+if(promedioGeneral > 6){
+    console.log("APROBADO");
+}else{
+    console.log("REPROBADO");
+}
